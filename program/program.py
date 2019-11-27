dfe1ed6b541870885324eb1308dd9be2d8d92e78
Alexander Tkalych <atkalych@valiantsolutions.com>
2019-11-27 14:45:55 -0500

    load

--- program/program.py
+++ program/program.py
@@ -523,8 +523,10 @@ def main():
     # instantiate the class
     l = LSTM_network()
 
+    print(l.training_dir)
+
     # # load the data
-    l.data_load('s3://blstm-cracker/test-run/train/train.csv')
+    # l.data_load(l.training_dir)
 
     # # get the dataset characteristics
     # l.parse_data()    
