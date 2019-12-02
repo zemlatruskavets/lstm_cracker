63e0bd266d8fce80da4db4d8eebf87edf9d47508
Alexander <ajtkalych@gmail.com>
2019-12-02 18:09:54 +0000

    added setup.py

---
+++ program/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(name='lstm_cracker',
+      version='1.0',
+      url='https://github.com/zemlatruskavets/lstm_cracker',
+      packages=find_packages(exclude=('tests', 'docs')))
