96bf4499d4d287f5bddf26879fb1834b872c7ae2
Alexander Tkalych <atkalych@valiantsolutions.com>
2019-12-04 09:35:37 -0500

    initialize all tf variables

--- program/program.py
+++ program/program.py
@@ -439,6 +439,7 @@ class LSTM_network():
         # save the model in /opt/ml/model
         # self.model.save('/opt/ml/model/1', save_format='tf')
         with tf.keras.backend.get_session() as sess:
+            sess.run(tf.global_variables_initializer())
             tf.saved_model.simple_save(
                 sess,
                 '/opt/ml/model/1',
