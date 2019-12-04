5f95bf3d648c3791a6b4e21ef31b2b3ed63aa65a
Alexander Tkalych <atkalych@valiantsolutions.com>
2019-12-04 10:18:17 -0500

    have to revert

--- program/program.py
+++ program/program.py
@@ -440,9 +440,11 @@ class LSTM_network():
         # self.model.save('/opt/ml/model/1', save_format='tf')
         with tf.keras.backend.get_session() as sess:
             sess.run(tf.global_variables_initializer())
-            tf.saved_model.save(
+            tf.saved_model.simple_save(
                 sess,
-                '/opt/ml/model/1')
+                '/opt/ml/model/1',
+                inputs={'input_image': self.model.input},
+                outputs={t.name: t for t in self.model.outputs})
         # sess = K.get_session()
         
         # tf.saved_model.simple_save(
