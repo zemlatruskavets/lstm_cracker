38a3b2631687eb5e4df1c45ed123b11b4a803112
Alexander Tkalych <atkalych@valiantsolutions.com>
2019-12-04 12:50:55 -0500

    get rid of additional model

--- program/program.py
+++ program/program.py
@@ -446,11 +446,11 @@ class LSTM_network():
         tf.saved_model.simple_save(
             sess,
             # os.environ['SM_MODEL_DIR'],
-            os.path.join(os.environ['SM_MODEL_DIR'], 'model/1'),
+            os.path.join(os.environ['SM_MODEL_DIR'], '1'),
             inputs={'inputs': self.model.input},
             outputs={t.name: t for t in self.model.outputs})
 
-        print('the model was supposedly put here: %s' % os.path.join(os.environ['SM_MODEL_DIR'], 'model/1'))
+        print('the model was supposedly put here: %s' % os.path.join(os.environ['SM_MODEL_DIR'], '1'))
 
 
         # tf.saved_model.save(self.model, os.environ['SM_MODEL_DIR'])
